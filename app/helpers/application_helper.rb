31f376f54197b7f780d7864f905505da41ca15e4
Michael Saint Clair <ceo.wonu@gmail.com>
2015-03-22 20:06:39 -0400

    Installed bootstrap and styled nav area

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def my_name
+    "Michael Saint Claire"
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Bloccit::Application.routes.draw do
+  #get "contact/contact"
+
   get "welcome/index"
   get "welcome/about"
+  get "welcome/contact"
 
       root to: 'welcome#index'
 end
