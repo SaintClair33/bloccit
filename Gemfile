85653748e8b7094756222ca53e2c5b2ecdcae8e2
Michael Saint Clair <ceo.wonu@gmail.com>
2015-04-05 19:27:23 -0400

    Commit changes from Authentication continued

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ end
 
 gem 'faker'
 gem 'devise'
+gem 'figaro', '1.0'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bloccit::Application.config.secret_token = 'ac5e6e82a54b9d4a29e7a847a3a88bc465b8fb7fc93f99462d6a0703ebea937405749f716ae99ed04b6c6a2e36975b1cb5759880b7a5cecfa4256b136bfeafd1'
+#Bloccit::Application.config.secret_token = 'ac5e6e82a54b9d4a29e7a847a3a88bc465b8fb7fc93f99462d6a0703ebea937405749f716ae99ed04b6c6a2e36975b1cb5759880b7a5cecfa4256b136bfeafd1'
+Bloccit::Application.config.secret_token = ENV['SECRET_KEY_BASE']
