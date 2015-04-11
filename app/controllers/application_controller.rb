abce591d2b24c201ec00a095312c2daa983f658c
Michael Saint Clair <ceo.wonu@gmail.com>
2015-04-11 15:03:31 -0400

    Had to go back and update changes made the first time. Yeah some bullshit I know

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-    before_action :configure_permitted_parameters,
+    before_filter :configure_permitted_parameters, :posts_controller?, :flash_attack,
     if: :devise_controller?
 
     protected
@@ -8,4 +8,13 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :name
     end
+
+    def posts_controller?
+      controller_name == 'posts'
+    end
+
+    def flash_attack
+
+    end
+
 end
