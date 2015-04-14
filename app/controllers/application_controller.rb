b42a46f1659c0064b6ec89f15fa353adea9afcfe
Michael Saint Clair <ceo.wonu@gmail.com>
2015-04-14 11:39:23 -0400

    Complete auhentication continued assignment minus the skip before action as my Railsv version doesn't have it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-    before_filter :configure_permitted_parameters
+    before_filter :configure_permitted_parameters, if: :devise_controller?
 
-
-    if: :devise_controller?
-
-      before_filter :flash_attack
+    before_filter :flash_attack
 
     protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :name
     end
-    
-
 
     #def posts_controller?
-      #controller_name == 'posts'
-      #flash[:notice] = "This better work."
+      #if controller_name == "posts"
+        #flash[:notice] = "This flash should work."
+      #end
     #end
+
     def flash_attack
-      if controller_name == 'posts'
-      flash[:notice] = "This better work."
+      if controller_name == "posts"
+      flash[:notice] = "This flash should work."
+      end
     end
-  end
-
-
-
-
-
-
 end
-
