f86c4fcb15ba0046a76d9e346b92c9ea10ae509e
Michael Saint Clair <ceo.wonu@gmail.com>
2015-04-14 10:45:33 -0400

    No change made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-    before_filter :configure_permitted_parameters, :flash_attack,
+    before_filter :configure_permitted_parameters
 
 
     if: :devise_controller?
+
+      before_filter :flash_attack
 
     protected
 
