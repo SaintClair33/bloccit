80080a92f202c63994b095131581f269c76a99d9
Michael Saint Clair <ceo.wonu@gmail.com>
2015-04-29 22:40:10 -0400

    Commit changes after meeting with Rizwan

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user #means the user table has the primary key and the post table is a foreign key to it
   default_scope {order('created_at DESC')}
-  #scope :ordered_by_title, -> { reorder(title: :ASC)}
-  #scope :ordered_by_reverse_created_at, -> { reorder(created_at: :DESC)}
+  scope :ordered_by_title, -> { reorder(title: :ASC)}
+  scope :ordered_by_reverse_created_at, -> { reorder(created_at: :DESC)}
 
   
 end
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,20 @@
 class PostPolicy < ApplicationPolicy
+    class Scope
+      attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all?
+      else
+        scope.where(:published => true)
+      end
+    end
+  end
   def index? # Overrides the false setting in Application_policy file. inheriting classes can overwrite
     #methods in parents class
     false
