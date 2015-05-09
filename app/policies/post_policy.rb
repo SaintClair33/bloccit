5d0a226abb13d05813c5b2e2004cfb56fbc62d3a
SaintClair33 <ceo.wonu@gmail.com>
2015-05-09 19:46:14 -0400

    Authorization checkpoint 310 before individual assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,7 @@
-class PostPolicy < ApplicationPolicy
+class PostPolicy < ApplicationPolicy #Policy for the post class
   def index? # Overrides the false setting in Application_policy file. inheriting classes can overwrite
     #methods in parents class
-    false
+    true
   end
 end
 
