8037707d88354097aff42064089545ccc5b7be57
SaintClair33 <ceo.wonu@gmail.com>
2015-05-30 19:39:30 -0400

    Checkpoint 313 completed validations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
-
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form_group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form_group'
+    end
+  end
   def my_name
     "Michael Saint Claire"
-
   end
+
 end
