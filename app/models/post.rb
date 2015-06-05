17dcf2d6e7bfcc14593ea2bbeecad82ddbbeb236
SaintClair33 <ceo.wonu@gmail.com>
2015-06-04 20:44:14 -0400

    Completed assignment for 314

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,25 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+
+  def markdown_title
+    (render_as_markdown).render(self.title).html_safe
+  end
+  
+
+  def markdown_body
+    (render_as_markdown).render(self.body).html_safe
+  end
+
+  private
+
+  def render_as_markdown
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    #return redcarpet
+  end
+  
   
 end
 
