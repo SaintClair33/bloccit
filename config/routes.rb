edd218749b257aca75d624cd98c0e9c2dc7ac86b
SaintClair33 <ceo.wonu@gmail.com>
2015-06-28 14:05:18 -0400

    Changed the routes but not finished yet

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,25 @@ Rails.application.routes.draw do
   get 'comments/create'
 
   devise_for :users
-
-  #resources :posts
-  #resources :topics
   #the resources method lets you restrict which RESTful routes you want
   resources :users, only: [:update] #creates new action users#update
-  resources :topics do
-    resources :posts, except: [:index] do
-      resource :summaries, only: [:create, :show]# use singular resource when you dont need an ID to look it up
-      resources :comments, only: [:create, :destroy]
-    end
 
+  #resources :topics do
+    #resources :posts, except: [:index] do
+      #resource :summaries, only: [:create, :show]# use singular resource when you dont need an ID to look it up
+      #resources :comments, only: [:create, :destroy]
+    #end
+
+  #end
+
+  resources :topics do
+    resources :posts, except: [:index]
   end
+
+  resources :posts, only: [] do # the [] we dont want to create any /posts/:id routes just posts/:post_id/comments
+    resource  :summaries, only: [:create, :show] # use singular resource when you dont need an ID to look it up
+    resources :comments, only: [:create, :destroy]
+  end   
   
   get 'about' => 'welcome#about'
 
