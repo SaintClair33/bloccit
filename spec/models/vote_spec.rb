3a8840f0f3d546a39e980e0fad78362d9a40cad7
SaintClair33 <ceo.wonu@gmail.com>
2015-06-29 17:19:49 -0400

    Completed rspec assignment

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,9 @@
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        expect(@post.up_votes).to eq(-1) || expect(@post.up.votes).to eq(1)
+      end
+    end
+  end
+end
